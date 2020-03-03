26eba67062b9b45ad35ab2cd5fe6efa00a11dec9
DmiKol <dmikol13@mail.ru>
2020-03-03 09:34:45 +0300

    feat: add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let obj = {};
+  bracketsConfig.forEach(function(item){
+      obj[item[0]] = item[1];
+  });
+  str.split('').forEach(function(item){
+      if(stack.length == 0){
+          stack.push(item);
+      }
+      else{
+          if(item == obj[stack[stack.length-1]]){
+              stack.pop();
+          }
+          else{
+            stack.push(item);
+          }
+      }
+      
+  });
+  return stack.length == 0 ? true : false;
 }
